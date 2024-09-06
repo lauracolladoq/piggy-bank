6e727c553470df3229ff52bafc9b9e7811cfb8e5
OussamaAtifii <oussamaati03@gmail.com>
2024-09-07 00:57:16 +0200

    chore: include daisyUI to tailwind conf

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,14 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        "primary": "#4AC397",
+        primary: "#4AC397",
         "primary-hover": "#41AB84",
-      }
+      },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
