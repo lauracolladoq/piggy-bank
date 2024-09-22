090782c2780417e15c83c2aab6e2e700aca3ce18
OussamaAtifii <oussamaati03@gmail.com>
2024-09-22 21:42:54 +0200

    refactor: improve user schema validations

--- backend/src/validations/user-schema.ts
+++ backend/src/validations/user-schema.ts
@@ -4,14 +4,26 @@ import { z } from 'zod';
 export const baseUserSchema = z.object({
   username: z
     .string()
-    .min(3, 'The username must be at least 3 characters long')
-    .max(16, 'The username cannot exceed 16 characters'),
+    .min(1, 'Username is required')
+    .min(3, 'Username must be at least 3 characters long')
+    .max(16, 'Username cannot exceed 16 characters'),
   firstName: z.string().optional(),
   lastName: z.string().optional(),
-  email: z.string().email('The email address in invalid'),
+  email: z
+    .string()
+    .min(1, 'Email is required')
+    .max(244, 'Email cannot exceed 244 characters')
+    .email('The email address in invalid')
+    .toLowerCase(),
   password: z
     .string()
-    .min(8, 'The password must be at least 8 characters long'),
+    .min(1, 'Password is required')
+    .min(8, 'Password must be at least 8 characters long')
+    .max(255, 'Password cannot exceed 255 characters')
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).*$/,
+      'Password must contain lowercase letters, uppercase letters, numbers, and symbols'
+    ),
   avatar: z.string().optional(),
 });
 
@@ -23,18 +35,10 @@ export const userRegisterSchema = baseUserSchema.pick({
 });
 
 // Reglas para el inicio de sesión.
-export const userLoginSchema = baseUserSchema
-  .pick({
-    email: true,
-    password: true,
-  })
-  .extend({
-    email: z
-      .string()
-      .email('The email address in invalid')
-      .min(1, 'The email is required'),
-    password: z.string().min(1, 'The password is required'),
-  });
+export const userLoginSchema = z.object({
+  email: z.string().toLowerCase(),
+  password: z.string(),
+});
 
 // Reglas para la actualización el perfil.
 export const userProfileUpdateSchema = baseUserSchema.partial();
