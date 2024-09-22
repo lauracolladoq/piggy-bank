f72c6b179d5885361464d4c34fe51f8a9a6bb251
OussamaAtifii <oussamaati03@gmail.com>
2024-09-22 21:42:45 +0200

    feat: add user routes

---
+++ backend/src/routes/user-routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import UserController from "../controllers/user-controler";
+
+const userRouter = Router();
+
+userRouter.get("/", UserController.getAllUsers);
+
+userRouter.post("/register", UserController.register);
+userRouter.post("/login", UserController.login);
+
+userRouter.get("/:id", UserController.getUserById);
+userRouter.patch("/:id", UserController.updateUser);
+userRouter.delete("/:id", UserController.deleteUser);
+
+export default userRouter;
